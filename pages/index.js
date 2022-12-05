fe0fa75c4d7de92ab36c40731c4c459a81dc4774
강지원 <goalden@dawin.xyz>
2022-12-05 16:30:06 +0900

    초기세팅

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
           Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
-        {/* <div className={styles.grid}>
+        <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -29,10 +29,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -42,11 +39,9 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
-        </div> */}
+        </div>
       </main>
 
       <footer className={styles.footer}>
