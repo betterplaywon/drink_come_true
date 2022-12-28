991fcd9b7e9a11d3c33685037ec5f6bc9a16819e
betterplaywon <kkgi1234@gmail.com>
2022-12-28 16:08:16 +0900

    ♻️ actionType import문 수정

--- reducers/post.js
+++ reducers/post.js
@@ -2,21 +2,13 @@ import shortId from 'shortid';
 import produce from 'immer';
 import faker from 'faker';
 
-import {
-  REMOVE_POST_REQUEST,
-  REMOVE_POST_SUCCESS,
-  REMOVE_POST_FAILURE,
-  ADD_COMMENT_REQUEST,
-  ADD_COMMENT_SUCCESS,
-  ADD_COMMENT_FAILURE,
-  ADD_POST_REQUEST,
-  ADD_POST_SUCCESS,
-  ADD_POST_FAILURE,
-} from '../actionType';
+import * as AT from '../actionType';
 
 export const initialState = {
   mainPosts: [],
   imagePaths: [],
+
+  isMorePosts: true,
 
   loadPostLoading: false,
   loadPostDone: false,
@@ -35,8 +27,8 @@ export const initialState = {
   addCommentError: null,
 };
 
-initialState.mainPosts = initialState.mainPosts.concat(
-  Array(5)
+export const generateDummyPost = number =>
+  Array(number)
     .fill()
     .map(() => ({
       id: shortId.generate(),
@@ -59,16 +51,17 @@ initialState.mainPosts = initialState.mainPosts.concat(
           content: faker.lorem.sentence(),
         },
       ],
-    })),
-);
+    }));
+
+// initialState.mainPosts = initialState.mainPosts.concat(generateDummyPost(10));
 
 export const addPost = data => ({
-  type: ADD_POST_REQUEST,
+  type: AT.ADD_POST_REQUEST,
   data,
 });
 
 export const addComment = data => ({
-  type: ADD_COMMENT_REQUEST,
+  type: AT.ADD_COMMENT_REQUEST,
   data,
 });
 
@@ -95,47 +88,62 @@ const dummyComment = data => ({
 const reducer = (state = initialState, action) =>
   produce(state, draft => {
     switch (action.type) {
-      case ADD_POST_REQUEST:
+      case AT.ADD_POST_REQUEST:
         draft.addPostLoading = true;
         draft.addPostDone = false;
         draft.addPostError = null;
         break;
-      case ADD_POST_SUCCESS:
+      case AT.ADD_POST_SUCCESS:
         draft.addPostLoading = false;
         draft.addPostDone = true;
         draft.mainPosts.unshift(dummyPost(action.data));
         break;
-      case ADD_POST_FAILURE:
+      case AT.ADD_POST_FAILURE:
         draft.addPostLoading = false;
         addPostError = action.error;
         break;
-      case REMOVE_POST_REQUEST:
+      case AT.LOAD_POST_REQUEST:
+        draft.loadPostLoading = true;
+        draft.loadPostDone = false;
+        draft.loadPostError = null;
+        break;
+      case AT.LOAD_POST_SUCCESS:
+        draft.loadPostLoading = false;
+        draft.loadPostDone = true;
+        draft.mainPosts = action.data.concat(draft.mainPosts);
+        draft.isMorePosts = draft.mainPosts.length < 40;
+        break;
+      case AT.LOAD_POST_FAILURE:
+        draft.loadPostLoading = false;
+        loadPostError = action.error;
+        break;
+      case AT.REMOVE_POST_REQUEST:
         draft.removePostLoading = true;
         draft.removePostDone = false;
         draft.removePostError = null;
         break;
-      case REMOVE_POST_SUCCESS:
+      case AT.REMOVE_POST_SUCCESS:
         draft.mainPosts = draft.mainPosts.filter(f => f.id !== action.data);
         draft.removePostLoading = false;
         draft.removePostDone = true;
         break;
-      case REMOVE_POST_FAILURE:
+      case AT.REMOVE_POST_FAILURE:
         draft.removePostLoading = false;
         draft.removePostError = action.error;
         break;
-      case ADD_COMMENT_REQUEST:
+      case AT.ADD_COMMENT_REQUEST:
         draft.addCommentLoading = true;
         draft.addCommentDone = false;
         draft.addCommentError = null;
         break;
-      case ADD_COMMENT_SUCCESS: {
+      case AT.ADD_COMMENT_SUCCESS: {
         const post = draft.mainPosts.find(x => x.id === action.data.postId);
         post.Comments.unshift(dummyComment(action.data.content));
         draft.addCommentLoading = false;
         draft.addCommentDone = true;
         break;
       }
-      case ADD_COMMENT_FAILURE:
+      case AT.ADD_COMMENT_FAILURE:
         draft.addCommentLoading = false;
         draft.addCommentError = action.error;
         break;
--- sagas/post.js
+++ sagas/post.js
@@ -1,19 +1,8 @@
-import { all, fork, takeLatest, delay, put } from 'redux-saga/effects';
-import {
-  ADD_POST_REQUEST,
-  ADD_POST_SUCCESS,
-  ADD_POST_FAILURE,
-  ADD_COMMENT_REQUEST,
-  ADD_COMMENT_SUCCESS,
-  ADD_COMMENT_FAILURE,
-  ADD_POST_TO_ME,
-  REMOVE_POST_OF_ME,
-  REMOVE_POST_REQUEST,
-  REMOVE_POST_SUCCESS,
-  REMOVE_POST_FAILURE,
-} from '../actionType';
+import { all, fork, takeLatest, delay, put, call, throttle } from 'redux-saga/effects';
+import * as AT from '../actionType';
 
 import shortId from 'shortid';
+import { generateDummyPost } from '../reducers/post';
 
 function addPostAPI(data) {
   axios.post('/api/post/test', data);
@@ -25,19 +14,19 @@ function* addPost(action) {
     yield delay(1000);
     const id = shortId.generate();
     yield put({
-      type: ADD_POST_SUCCESS,
+      type: AT.ADD_POST_SUCCESS,
       data: {
         id,
         content: action.data,
       },
     });
     yield put({
-      type: ADD_POST_TO_ME,
+      type: AT.ADD_POST_TO_ME,
       data: id,
     });
   } catch (error) {
     yield put({
-      type: ADD_POST_FAILURE,
+      type: AT.ADD_POST_FAILURE,
       data: error.response,
     });
   }
@@ -52,54 +41,79 @@ function* removePost(action) {
   try {
     yield delay(1000);
     yield put({
-      type: REMOVE_POST_SUCCESS,
+      type: AT.REMOVE_POST_SUCCESS,
       data: action.data,
     });
     yield put({
-      type: REMOVE_POST_OF_ME,
+      type: AT.REMOVE_POST_OF_ME,
       data: action.data,
     });
   } catch (error) {
     yield put({
-      type: REMOVE_POST_FAILURE,
+      type: AT.REMOVE_POST_FAILURE,
       data: error.response,
     });
   }
 }
 
 function addCommentAPI(data) {
-  axios.post(`/api/post/${data.postId}/comment`, data);
+  axios.post(`/api/post/`, data);
 }
 
 function* addComment(action) {
   console.log(action);
-  //   const result = yield call(addaddCommentAPIAPI, action.data);
+  //   const result = yield call(addCommentAPI, action.data);
   try {
+    const id = shortId.generate();
     yield delay(1000);
     yield put({
-      type: ADD_COMMENT_SUCCESS,
-      data: action.data,
+      type: AT.ADD_COMMENT_SUCCESS,
+      data: generateDummyPost(10),
     });
   } catch (error) {
     yield put({
-      type: ADD_COMMENT_FAILURE,
+      type: AT.ADD_COMMENT_FAILURE,
+      data: error.response,
+    });
+  }
+}
+
+function loadPostAPI() {
+  axios.post(`/api/post/${data.postId}/load`, data);
+}
+
+function* loadPost(action) {
+  // const result = yield call(loadPostAPI, action.data);
+  try {
+    yield delay(1000);
+    yield put({
+      type: AT.LOAD_POST_SUCCESS,
+      data: generateDummyPost(10),
+    });
+  } catch (error) {
+    yield put({
+      type: AT.LOAD_POST_FAILURE,
       data: error.response,
     });
   }
 }
 
 function* watchAddPost() {
-  yield takeLatest(ADD_POST_REQUEST, addPost);
+  yield takeLatest(AT.ADD_POST_REQUEST, addPost);
 }
 
 function* watchAddComment() {
-  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
+  yield takeLatest(AT.ADD_COMMENT_REQUEST, addComment);
 }
 
 function* watchRemoveComment() {
-  yield takeLatest(REMOVE_POST_REQUEST, removePost);
+  yield takeLatest(AT.REMOVE_POST_REQUEST, removePost);
+}
+
+function* watchLoadPost() {
+  yield throttle(5000, AT.LOAD_POST_REQUEST, loadPost);
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost), fork(watchAddComment), fork(watchRemoveComment)]);
+  yield all([fork(watchAddPost), fork(watchAddComment), fork(watchRemoveComment), fork(watchLoadPost)]);
 }
