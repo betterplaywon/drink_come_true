c8fd0591bab2a7bfec4f18cb3104781f84722585
betterplaywon <kkgi1234@gmail.com>
2023-01-16 16:08:20 +0900

    🎨 로그인 유무에 따른 그래프 컴포넌트 활성화/비활성화 처리

--- pages/cycle.js
+++ pages/cycle.js
@@ -17,21 +17,27 @@ const cycle = () => {
   const dispatch = useDispatch();
   const { user } = useSelector(state => state.user);
 
-  useEffect(() => {
-    if (!(user && user.id)) {
-      Router.push('/');
-    }
-  }),
-    [user && user.id];
-
   return (
     <>
       <Head>
         <title>음주 주기 확인</title>
       </Head>
-      <DrinkChart />
+      <AppLayout>{user ? <DrinkChart /> : <p>'로그인 후 이용 가능합니다'</p>}</AppLayout>
     </>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async context => {
+  // context 안에 store가 들어있다.
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  context.store.dispatch({ type: AT.LOAD_MY_INFO_REQUEST });
+  // context.store.dispatch({ type: AT.LOAD_POSTS_REQUEST });
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+});
 
 export default cycle;
