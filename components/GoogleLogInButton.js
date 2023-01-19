1d09e1ce649ca86517a63b736f8c0d1a16978c2b
betterplaywon <kkgi1234@gmail.com>
2023-01-20 00:11:45 +0900

    🔥 불필요한 변수와 console.log 정리 & sass 설치

--- components/GoogleLogInButton.js
+++ components/GoogleLogInButton.js
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react';
 import { useSession, signIn } from 'next-auth/react';
 
 const GoogleLogInButton = () => {
-  const { data, status } = useSession();
-  console.log('로그인에서의 구글데이터: ', data);
   return (
     <>
       <p>Not signed in</p>
