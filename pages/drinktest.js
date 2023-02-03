bf54d4feda3a9514f771d86806bfa1dd1a131fa8
betterplaywon <kkgi1234@gmail.com>
2023-02-03 19:01:02 +0900

    🎨 술 테스트 페이지 getServersideProps 적용 & 디자인 적용

--- pages/drinktest.js
+++ pages/drinktest.js
@@ -2,6 +2,10 @@ import React from 'react';
 import AppLayout from '../components/AppLayout';
 import { Menu, Input, Row, Col, Layout, Breadcrumb, Image, Button } from 'antd';
 import Link from 'next/link';
+import wrapper from '../store/configureStore';
+import axios from 'axios';
+import { END } from 'redux-saga';
+import * as AT from '../actionType';
 import style from '../styles/drinkTestMain.module.css';
 
 const drinkTest = () => {
@@ -30,5 +34,17 @@ const drinkTest = () => {
     </AppLayout>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async context => {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  context.store.dispatch({ type: AT.LOAD_MY_INFO_REQUEST });
+  context.store.dispatch({ type: AT.LOAD_POSTS_REQUEST });
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+});
 
 export default drinkTest;
