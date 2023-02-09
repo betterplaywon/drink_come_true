0a69da02d2f9727031d393f18c837d113b7396c5
betterplaywon <kkgi1234@gmail.com>
2023-02-09 14:33:28 +0900

    🔥 사용하지 않는 import 삭제

--- reducers/post.js
+++ reducers/post.js
@@ -1,7 +1,4 @@
-import shortId from 'shortid';
 import produce from 'immer';
-import faker from 'faker';
-
 import * as AT from '../actionType';
 
 export const initialState = {
@@ -44,32 +41,6 @@ export const initialState = {
   imageUploadError: null,
 };
 
-// export const generateDummyPost = number =>
-//   Array(number)
-//     .fill()
-//     .map(() => ({
-//       id: shortId.generate(),
-//       User: {
-//         id: shortId.generate(),
-//         name: faker.name.findName(),
-//       },
-//       content: faker.lorem.paragraph(),
-//       Images: [
-//         {
-//           src: faker.image.sports(),
-//         },
-//       ],
-//       Comments: [
-//         {
-//           User: {
-//             id: shortId.generate(),
-//             name: faker.name.findName(),
-//           },
-//           content: faker.lorem.sentence(),
-//         },
-//       ],
-//     }));
-
 export const addPost = data => ({
   type: AT.ADD_POST_REQUEST,
   data,
@@ -79,26 +50,6 @@ export const addComment = data => ({
   type: AT.ADD_COMMENT_REQUEST,
   data,
 });
-
-// const dummyPost = data => ({
-//   id: data.id,
-//   content: data.content,
-//   User: {
-//     id: 1,
-//     name: '소주소주소주수조소주',
-//   },
-//   Images: [],
-//   Comments: [],
-// });
-
-// const dummyComment = data => ({
-//   id: shortId.generate(),
-//   content: data,
-//   User: {
-//     id: 1,
-//     name: '그라가스',
-//   },
-// });
 
 const reducer = (state = initialState, action) =>
   produce(state, draft => {
