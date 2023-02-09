179619ff40b679e0247bf851381671a384653766
betterplaywon <kkgi1234@gmail.com>
2023-02-09 14:35:18 +0900

    🔥 UserProfile, profile > deprecated 처리

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,67 +1,73 @@
-import React, { useCallback } from 'react';
-import { Avatar, Card, Button } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
-import { LOG_OUT_REQUEST } from '../actionType';
-import Link from 'next/link';
+/**
+ * @deprecated
+ * 2023.02.09
+ * 사용하지 않는 컴포넌트
+ */
 
-import GoogleLogOutButton from './GoogleLogOutButton';
-import { useSession } from 'next-auth/react';
+// import React, { useCallback } from 'react';
+// import { Avatar, Card, Button } from 'antd';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { LOG_OUT_REQUEST } from '../actionType';
+// import Link from 'next/link';
 
-const UserProfile = () => {
-  const dispatch = useDispatch();
-  const { Meta } = Card;
-  const { user, logOutLoading } = useSelector(state => state.user);
-  const { data, status } = useSession();
+// import GoogleLogOutButton from './GoogleLogOutButton';
+// import { useSession } from 'next-auth/react';
 
-  const handleLogout = useCallback(() => {
-    dispatch({ type: LOG_OUT_REQUEST });
-  }, []);
+// const UserProfile = () => {
+//   const dispatch = useDispatch();
+//   const { Meta } = Card;
+//   const { user, logOutLoading } = useSelector(state => state.user);
+//   const { data } = useSession();
 
-  return (
-    <>
-      {user && !data ? (
-        <Card
-          cover={<img alt="drinkGroup" src="https://platum.kr/wp-content/uploads/2017/11/thebooth2-1024x683.jpg" />}
-          actions={[
-            <div key="twit">
-              게시글
-              <br /> {user.Posts.length}
-            </div>,
-            <div key="followers">
-              <Link href="/profile">
-                <a>
-                  팔로워
-                  <br /> {user.Followers.length}
-                </a>
-              </Link>
-            </div>,
-            <div key="followings">
-              <Link href="/profile">
-                <a>
-                  팔로잉
-                  <br /> {user.Followings.length}
-                </a>
-              </Link>
-            </div>,
-          ]}
-        >
-          <Meta
-            avatar={<Avatar>{user.nickname[0]}</Avatar>}
-            title={user.nickname}
-            description="오늘은 술을 얼마나 마셨나요"
-            style={{ marginBottom: '10px' }}
-          />
-          <Button onClick={handleLogout} loading={logOutLoading}>
-            로그아웃
-          </Button>
-        </Card>
-      ) : (
-        <Card>
-          <GoogleLogOutButton />
-        </Card>
-      )}
-    </>
-  );
-};
+//   const handleLogout = useCallback(() => {
+//     dispatch({ type: LOG_OUT_REQUEST });
+//   }, []);
 
-export default UserProfile;
+//   return (
+//     <>
+//       {user ? (
+//         <Card
+//           cover={<img alt="drinkGroup" src="https://platum.kr/wp-content/uploads/2017/11/thebooth2-1024x683.jpg" />}
+//           actions={[
+//             <div key="twit">
+//               게시글
+//               <br /> {user.Posts.length}
+//             </div>,
+//             <div key="followers">
+//               <Link href="/profile">
+//                 <a>
+//                   친구들
+//                   <br /> {user.Followers.length}
+//                 </a>
+//               </Link>
+//             </div>,
+//             <div key="followings">
+//               <Link href="/profile">
+//                 <a>
+//                   팬들
+//                   <br /> {user.Followings.length}
+//                 </a>
+//               </Link>
+//             </div>,
+//           ]}
+//         >
+//           <Meta
+//             avatar={<Avatar>{user.nickname[0]}</Avatar>}
+//             title={user.nickname}
+//             description="오늘은 술을 얼마나 마셨나요"
+//             style={{ marginBottom: '10px' }}
+//           />
+//           <Button onClick={handleLogout} loading={logOutLoading}>
+//             로그아웃
+//           </Button>
+//         </Card>
+//       ) : (
+//         <Card>
+//           <GoogleLogOutButton />
+//         </Card>
+//       )}
+//     </>
+//   );
+// };
+
+// export default UserProfile;
--- pages/profile.js
+++ pages/profile.js
@@ -1,107 +1,113 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import Head from 'next/head';
-import AppLayout from '../components/AppLayout';
-import FollowList from '../components/FollowList';
-import NicknameForm from '../components/NicknameForm';
-import { useSelector, useDispatch } from 'react-redux';
-import * as AT from '../actionType';
+/**
+ * @deprecated
+ * 2023.02.09
+ * 활용도가 떨어지므로 deprecated
+ */
 
-import wrapper from '../store/configureStore';
-import axios from 'axios';
-import { END } from 'redux-saga';
-import Router from 'next/router';
-import useSWR from 'swr';
+// import React, { useCallback, useEffect, useState } from 'react';
+// import Head from 'next/head';
+// import AppLayout from '../components/AppLayout';
+// import FollowList from '../components/FollowList';
+// import NicknameForm from '../components/NicknameForm';
+// import { useSelector, useDispatch } from 'react-redux';
+// import * as AT from '../actionType';
 
-import { useSession } from 'next-auth/react';
+// import wrapper from '../store/configureStore';
+// import axios from 'axios';
+// import { END } from 'redux-saga';
+// import Router from 'next/router';
+// import useSWR from 'swr';
 
-const profile = () => {
-  const dispatch = useDispatch();
-  const { user } = useSelector(state => state.user);
-  const [followersLimit, setFollowersLimit] = useState(3);
-  const [followingsLimit, setFollowingsLimit] = useState(3);
+// import { useSession } from 'next-auth/react';
 
-  const { data } = useSession();
+// const profile = () => {
+//   const dispatch = useDispatch();
+//   const { user } = useSelector(state => state.user);
+//   const [followersLimit, setFollowersLimit] = useState(3);
+//   const [followingsLimit, setFollowingsLimit] = useState(3);
 
-  const fetcher = url => axios.get(url, { withCredentials: true }).then(result => result.data);
+//   const { data } = useSession();
 
-  const { data: followersData, error: followersError } = useSWR(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/user/followers?limit=${followersLimit}`,
-    fetcher,
-  );
-  const { data: followingsData, error: followingsError } = useSWR(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/user/followings?limit=${followingsLimit}`,
-    fetcher,
-  );
+//   const fetcher = url => axios.get(url, { withCredentials: true }).then(result => result.data);
 
-  useEffect(() => {
-    dispatch({
-      type: AT.LOAD_FOLLOWERS_REQUEST,
-    });
-    dispatch({
-      type: AT.LOAD_FOLLOWINGS_REQUEST,
-    });
-  }, []);
+//   const { data: followersData, error: followersError } = useSWR(
+//     `${process.env.NEXT_PUBLIC_BASE_URL}/user/followers?limit=${followersLimit}`,
+//     fetcher,
+//   );
+//   const { data: followingsData, error: followingsError } = useSWR(
+//     `${process.env.NEXT_PUBLIC_BASE_URL}/user/followings?limit=${followingsLimit}`,
+//     fetcher,
+//   );
 
-  useEffect(() => {
-    if (!(user && user.id && data)) {
-      Router.push('/');
-    }
-  }),
-    [user && user.id, data];
+//   useEffect(() => {
+//     dispatch({
+//       type: AT.LOAD_FOLLOWERS_REQUEST,
+//     });
+//     dispatch({
+//       type: AT.LOAD_FOLLOWINGS_REQUEST,
+//     });
+//   }, []);
 
-  const moreViewFollowings = useCallback(() => {
-    setFollowingsLimit(prev => prev + 3);
-  }, []);
+//   useEffect(() => {
+//     if (!(user && user.id && data)) {
+//       Router.push('/');
+//     }
+//   }),
+//     [user && user.id, data];
 
-  const moreViewFollowers = useCallback(() => {
-    setFollowersLimit(prev => prev + 3);
-  }, []);
+//   const moreViewFollowings = useCallback(() => {
+//     setFollowingsLimit(prev => prev + 3);
+//   }, []);
 
-  if (!user || !data) {
-    return <div>'LOADING'</div>;
-  }
+//   const moreViewFollowers = useCallback(() => {
+//     setFollowersLimit(prev => prev + 3);
+//   }, []);
 
-  return (
-    <>
-      <Head>
-        <title>프로필 타이틀</title>
-      </Head>
-      {user && !data ? (
-        <AppLayout>
-          <NicknameForm />
-          <FollowList
-            header="팔로잉"
-            data={followingsData}
-            handleMoreView={moreViewFollowings}
-            loading={!followingsData && !followingsError}
-          />
-          <FollowList
-            header="팔로워"
-            data={followersData}
-            handleMoreView={moreViewFollowers}
-            loading={!followersData && !followersError}
-          />
-        </AppLayout>
-      ) : (
-        <AppLayout>
-          <div>소셜 로그인 프로필</div>
-        </AppLayout>
-      )}
-    </>
-  );
-};
+//   if (!user || !data) {
+//     return <div>'LOADING'</div>;
+//   }
 
-export const getServerSideProps = wrapper.getServerSideProps(async context => {
-  const cookie = context.req ? context.req.headers.cookie : '';
-  axios.defaults.headers.Cookie = '';
-  if (context.req && cookie) {
-    axios.defaults.headers.Cookie = cookie;
-  }
-  context.store.dispatch({ type: AT.LOAD_MY_INFO_REQUEST });
-  // context.store.dispatch({ type: AT.LOAD_POSTS_REQUEST });
-  context.store.dispatch(END);
+//   return (
+//     <>
+//       <Head>
+//         <title>프로필 타이틀</title>
+//       </Head>
+//       {user && !data ? (
+//         <AppLayout>
+//           <NicknameForm />
+//           <FollowList
+//             header="팔로잉"
+//             data={followingsData}
+//             handleMoreView={moreViewFollowings}
+//             loading={!followingsData && !followingsError}
+//           />
+//           <FollowList
+//             header="팔로워"
+//             data={followersData}
+//             handleMoreView={moreViewFollowers}
+//             loading={!followersData && !followersError}
+//           />
+//         </AppLayout>
+//       ) : (
+//         <AppLayout>
+//           <div>소셜 로그인 프로필</div>
+//         </AppLayout>
+//       )}
+//     </>
+//   );
+// };
 
-  await context.store.sagaTask.toPromise();
-});
+// export const getServerSideProps = wrapper.getServerSideProps(async context => {
+//   const cookie = context.req ? context.req.headers.cookie : '';
+//   axios.defaults.headers.Cookie = '';
+//   if (context.req && cookie) {
+//     axios.defaults.headers.Cookie = cookie;
+//   }
+//   context.store.dispatch({ type: AT.LOAD_MY_INFO_REQUEST });
+//   // context.store.dispatch({ type: AT.LOAD_POSTS_REQUEST });
+//   context.store.dispatch(END);
 
-export default profile;
+//   await context.store.sagaTask.toPromise();
+// });
+
+// export default profile;
