185af8936f52c848d3e4b245be41eb7b60ee57fb
betterplaywon <kkgi1234@gmail.com>
2023-03-04 20:58:54 +0900

    🎨 custom hook ts 적용, 술 테스트 목데이터 js 임시 변경

--- hooks/useInput.ts
+++ hooks/useInput.ts
@@ -1,10 +1,10 @@
-import { useState, useCallback, SetStateAction, Dispatch } from 'react';
+import React, { useState, useCallback, SetStateAction, Dispatch } from 'react';
 
-type ReturnType<T> = [T, (e: boolean) => void, Dispatch<SetStateAction<T>>];
+type ReturnType<T> = [T, (e: any) => void, Dispatch<SetStateAction<T>>];
 export default <T extends string>(initValue: T): ReturnType<T> => {
-  const [userInfo, setUserInfo] = useState(initValue);
+  const [userInfo, setUserInfo] = useState<string>(initValue);
 
-  const handleUserInfo = useCallback((e) => {
+  const handleUserInfo = useCallback((e): void => {
     setUserInfo(e.target.value);
   }, []);
 
